7be67412f1636b3031c55dfd2cdde2399144503c
Caio Emannuel <caioemannueldiniz@gmail.com>
2024-02-28 19:25:55 -0300

    Editando uma pessoa

--- src/services/people.ts
+++ src/services/people.ts
@@ -47,3 +47,21 @@ export const add = async (data: PeopleCreateData) => {
     return false;
   }
 };
+
+type UpdateFilters = { id?: number; id_event: number; id_group?: number };
+type PeopleUpdateData = Prisma.Args<
+  typeof prisma.eventPeople,
+  "update"
+>["data"];
+
+export const update = async (
+  filters: UpdateFilters,
+  data: PeopleUpdateData
+) => {
+  try {
+    return await prisma.eventPeople.updateMany
+    ({where: filters, data})
+  } catch (err) {
+    return false;
+  }
+};
