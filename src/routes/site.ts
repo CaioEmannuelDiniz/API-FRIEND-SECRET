4f9847006b0ee570f0a1a5206bf76b2aa8a566ab
Caio Emannuel <caioemannueldiniz@gmail.com>
2024-02-29 23:31:37 -0300

    Buscando a pessoa pelo cpf

--- src/routes/site.ts
+++ src/routes/site.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import * as events from "../controllers/events";
+import * as people from "../controllers/people";
 
 const router = Router();
 
@@ -8,6 +9,9 @@ router.get("/ping", (req, res) => {
   res.json({ pong: true });
 });
 
+//localizar o evento
 router.get("/events/:id", events.getEvent);
+//localizar uma pessoa pelo cpf
+router.get("/events/:id_event/search", people.searchPerson);
 
 export default router;
