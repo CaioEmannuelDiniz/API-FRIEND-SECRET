edd3514572dc8bc762aae0263d1f7142b22e8b18
Caio Emannuel <caioemannueldiniz@gmail.com>
2024-03-01 10:37:07 -0300

    Configurações finais sistema pronto para produção

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express from "express";
 import cors from "cors";
 import https from "https";
 import http from "http";
+import fs from "fs";
 
 import siteRoutes from "./routes/site";
 import adminRoutes from "./routes/admin";
@@ -31,8 +32,20 @@ const runServer = (port: number, server: http.Server) => {
 
 const regularServer = http.createServer(app);
 if (process.env.NODE_ENV === "production") {
-  //TODO: configurar SSL
-  //TODO: rodar server na 80 e na 443
+  //configurar SSL
+  const options = {
+    key: fs.readFileSync(process.env.SSL_KEY as string),
+    cert: fs.readFileSync(process.env.SSL_CERT as string),
+  };
+
+  //rodar server na 80 e na 443
+  const secServer = https.createServer(options, app);
+
+  //server de desenvolvimento
+  runServer(80, regularServer);
+
+  //server que roda o servidor seguro
+  runServer(443, secServer);
 } else {
   const serverPort: number = process.env.PORT
     ? parseInt(process.env.PORT)
