a7f03d9ed83597d9f90b08a9a8f84815defeedf9
Caio Emannuel <caioemannueldiniz@gmail.com>
2024-02-29 23:24:06 -0300

    Corrigindo erro na encrypt dos matched

--- src/utils/match.ts
+++ src/utils/match.ts
@@ -1,7 +1,5 @@
-import { match } from "assert";
-
-export const encrypyMatch = (id: number): string => {
-  return `${process.env.DEFAULT_TOKEN}${process.env.DEFAULT_TOKEN}`;
+export const encryptMatch = (id: number): string => {
+  return `${process.env.DEFAULT_TOKEN}${id}${process.env.DEFAULT_TOKEN}`;
 };
 
 export const decryptMatch = (match: string): number => {
